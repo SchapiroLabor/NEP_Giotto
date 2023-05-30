c0ed97c2041f78e8669ad6c730c6e6dd08e65c00
Chiara Schiller <chiara_schiller@t-online.de>
2023-05-30 13:55:57 +0200

    parallelize Giotto run and add Risom breast analysis

--- src/Giotto_SCNA.R
+++ src/Giotto_SCNA.R
@@ -4,6 +4,9 @@ library(Giotto)
 library(reticulate)
 library(readr)
 library(tidyverse)
+library(foreach)
+library(doParallel)
+
 
 getwd()
 # Giotto and python configurations
@@ -16,61 +19,62 @@ Sys.setenv(RETICULATE_PYTHON = "/Users/chiaraschiller/Library/r-miniconda-arm64/
 
 # load simulated data
 
-files = list.files("./../../../../data/Sim_100_equal4/", pattern = ".csv")
-data_path = "./../../../../data/Sim_100_equal4/"
+files = list.files("./../../../../data/Sim_100_asym_01/", pattern = ".csv")
+data_path = "./../../../../data/Sim_100_asym_01/"
 data_list = list()
 
-for (i in files){
-  print(i)
-data_list[[i]] <- readr::read_csv(paste0(data_path,i))
-}
 
-giotto_list = list()
-x=0
+# Set the number of cores to use
+num_cores <- 4
+cl <- makeCluster(num_cores)
+registerDoParallel(cl)
+
+
 # create dataframe with random values to pretend to have expression data to generate a giotto object
-for (i in data_list){
-  x = x+1
-  df = data.frame(cbind(rep(1,nrow(i)), rep(1,nrow(i))))
+#for (i in data_list){
+foreach(i = 1:length(files), .packages = c("Giotto","reticulate", "tidyverse")) %dopar%{
+  data_list <- readr::read_csv(paste0(data_path,files[i]))
+  df = data.frame(cbind(rep(1,nrow(data_list)), rep(1,nrow(data_list))))
   #df <- data.frame(matrix(runif(nrow(data_list[[i]]) * 2), nrow = nrow(data_list[[i]])))
   df$cell_ID = seq(1, nrow(df), by = 1)
   colnames(df) = c("M1", "M2", "cell_ID")
 
-  metadata = cbind(as.character(i$ct), seq(1, nrow(df), by = 1))
+  metadata = cbind(as.character(data_list$ct), seq(1, nrow(df), by = 1))
   colnames(metadata) = c("ct", "cell_ID")
   # colbind the two datasets
   #data = cbind(df, as.data.frame(data))
   data = createGiottoObject(raw_exprs = as.data.frame(t(df)),
-                          spatial_locs = as.data.frame(i[, !(names(i) %in% c("ct"))]),
+                          spatial_locs = as.data.frame(data_list[, !(names(data_list) %in% c("ct"))]),
                           #instructions = my_instructions,
                           cell_metadata = metadata)
 
   data <- normalizeGiotto(gobject = data)
   # create network (required for binSpect methods)
-  giotto_list[[files[x]]] = createSpatialNetwork(gobject = data, minimum_k = 2)
+  giotto_list = createSpatialNetwork(gobject = data, minimum_k = 2,
+                                                  method = "Delaunay",
+                                                  #method = "kNN",
+                                                  #k = 10,
+                                                  )
+  
+  cell_proximities = cellProximityEnrichment(gobject = giotto_list,
+                                             cluster_column = 'ct',
+                                             spatial_network_name = 'Delaunay_network',
+                                             #spatial_network_name = 'kNN_network',
+                                             adjust_method = 'fdr',
+                                             number_of_simulations = 100)
+  
+  cell_PI= separate(cell_proximities[[2]], unified_int, into = c("label1", "label2"), sep = "--")
+} -> cell_PI_list
+
+for(i in 1:length(cell_PI_list)){
+  cell_PI_list[[i]]$sample = rep(files[i], nrow(cell_PI_list[[i]]))
 }
 
-# calculate cell proximities
-# PI value is the proximity index of the feature in the cluster
-cell_PI=list()
-x=0
-
-for (i in giotto_list){
-  x=x+1
-  cell_proximities = cellProximityEnrichment(gobject = i,
-                        cluster_column = 'ct',
-                        spatial_network_name = 'Delaunay_network',
-                        adjust_method = 'fdr',
-                        number_of_simulations = 1000)
-
-  cell_PI[[files[x]]] = separate(cell_proximities[[2]], unified_int, into = c("label1", "label2"), sep = "--")
-  #names(cell_PI)[i]=gsub("\\..*", "", files[i])
-  write.csv(cell_PI[[files[x]]],file=paste0("./../../output/sim100_equal4_",gsub("\\..*", "", files[x]) ,"_proximities.csv"),row.names = TRUE)
-}
 
 ## create matrix for comparison
-data = do.call("rbind", cell_PI)
+data = do.call("rbind", cell_PI_list)
 
-data$sample <- gsub("\\..*","",rownames(data))
+data$sample <- sub(".csv.[0-9]*$", "", data$sample)
 
 data = data %>% select(label1, label2, PI_value, sample)
 
@@ -82,6 +86,4 @@ data = spread(data, key = key, value = PI_value)
 rownames(data) = data$sample
 data = data[,-1]
 
-write.csv(data,"./../../../Comparison/results/Giotto_sim100_equal4.csv")
-
-
+write.csv(data,"./../../../Comparison/results_4ct_cross01/Giotto_delaunay_4ct_cross01.csv")
